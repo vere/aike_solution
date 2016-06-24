32d6d973cf09a66b8abf50c2275aeb35ea6643bc
vere <verezhang@gmail.com>
2016-06-24 18:58:02 +0800

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,92 @@
+
+
+$(document).ready(function() { 
+
+	//Animated scrolling		   
+	$('ul.mainmenu a').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var $target = $(this.hash);
+			$target = $target.length && $target || $('[id=' + this.hash.slice(1) +']');
+			if ($target.length) {
+				$('ul.mainmenu li').removeClass('active');
+				$(this).parent('li').addClass('active');
+				var targetOffset = $target.offset().top-200;
+				$('html,body').animate({scrollTop: targetOffset}, 1000);
+				return false;
+			}
+		}
+	});
+
+	// Scroll Down Link
+	$('.scrolldown').click(function() {
+		var targetOffset = $('div.blockquote').offset().top-80;
+		$('html,body').animate({scrollTop: targetOffset}, 1000);
+	});
+
+	// Menu Scroll Hide
+	var nav = $('.menubar');
+	var scroll = $('.menubar').attr('data-scroll');
+	$(function(){
+		$('.menubar').data('size','big');
+		if (scroll == 'false') {
+			nav.css({
+				marginTop:'0px'
+			});
+		};
+	});
+	$(window).scroll(function(){
+		if ($('body').scrollTop() > 0 && scroll == 'true') {
+			if (nav.data('size') == 'big') {
+				nav.data('size','small').stop().animate({
+					marginTop:'0px'
+				}, 300);
+			}
+		} else {
+			if (nav.data('size') == 'small' && scroll == 'true') {
+				nav.data('size','big').stop().animate({
+					marginTop:'-100px'
+				}, 300);
+			}
+			
+		}
+	});
+
+	// Text Rotator
+	$('.rotate').each(function(){
+		var el = $(this);
+		var text = $(this).html().split(",");
+		el.html(text[0]);
+		setInterval(function() {
+			el.animate({
+              textShadowBlur:20,
+              opacity: 0
+            }, 500 , function() {
+              index = $.inArray(el.html(), text)
+              if((index + 1) == text.length) index = -1
+              el.text(text[index + 1]).animate({
+                textShadowBlur:0,
+                opacity: 1
+              }, 500 );
+            });
+		}, 2000);
+	});
+
+
+
+	// Blog Post Open
+	$('a.openpost').click(function(){
+		var id = $(this).attr('data-id');
+		$('#snippets').hide();
+		$('article.full[data-id="'+id+'"]').show();
+		$('#postsfull').fadeIn();
+	});
+	$('a.backtoblog').click(function(){
+		$('#postsfull').hide();
+		$('article.full').hide();
+		$('#snippets').fadeIn();
+	});
+
+
+
+
+});
