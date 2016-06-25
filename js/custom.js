75c6ca6896a016d758c858f1fa8989c230b2c630
vere <verezhang@gmail.com>
2016-06-26 00:22:13 +0800

    fix scroll

--- js/custom.js
+++ js/custom.js
@@ -16,12 +16,6 @@ $(document).ready(function() {
 			}
 		}
 	});
-	// Scroll Down Link
-	$('.scrolldown').click(function() {
-		var targetOffset = $('div.blockquote').offset().top-80;
-		$('html,body').animate({scrollTop: targetOffset}, 1000);
-	});
-
 
 	// Menu Scroll Hide
 	var nav = $('.menubar');
